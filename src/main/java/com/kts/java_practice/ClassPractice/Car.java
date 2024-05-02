5e730407fe8c7ae0bdea03b12b476375bee89938
Kim tae-seop <kts3445@naver.com>
2024-05-02 16:38:12 +0900

    feat : Class Overriding Practice 클래스에서 생성자 오버로딩 연습

--- src/main/java/com/kts/java_practice/ClassPractice/Car.java
+++ src/main/java/com/kts/java_practice/ClassPractice/Car.java
@@ -3,7 +3,7 @@ package com.kts.java_practice.ClassPractice;
 public class Car {
     // 필드에서 객체 초기화
     String company = "현대";
-    String name;
+    String model;
     String color;
     int cc;
 
@@ -14,4 +14,23 @@ public class Car {
     }
     //  color = color; 로 하면 필드와 매개변수 명이 동일하기 때문에 접근 불가
     // => 동일한 이름의 매개 변수가 사용 우선순위가 높기 때문
+
+
+    // 오버로딩. 여러개의 생성자 선언
+    Car() {}
+
+    Car(String model) {
+        this.model = model;
+    }
+
+    Car(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+
+    Car(String model, String color, int cc){
+        this.model = model;
+        this.color = color;
+        this.cc=cc;
+    }
 }
--- src/main/java/com/kts/java_practice/ClassPractice/CarExample.java
+++ src/main/java/com/kts/java_practice/ClassPractice/CarExample.java
@@ -6,5 +6,27 @@ public class CarExample {
         Car myCar = new Car ("검정", 3000);
         myCar.color = "white";
         System.out.println(myCar.color);
+
+        // 오버로딩 실습
+        Car car1 = new Car();
+        System.out.println("회사 : " + car1.company);
+        System.out.println();
+
+        Car car2 = new Car("자가용");
+        System.out.println("회사 : " + car2.company);
+        System.out.println("모델 : " + car2.model);
+        System.out.println();
+
+        Car car3 = new Car("자가용", "검정");
+        System.out.println("회사 : " + car3.company);
+        System.out.println("모델 : " + car3.model);
+        System.out.println("색상 : " + car3.color);
+        System.out.println();
+
+        Car car4 = new Car("택시", "흰색", 1000);
+        System.out.println("회사 : " + car4.company);
+        System.out.println("모델 : " + car4.model);
+        System.out.println("색상 : " + car4.color);
+        System.out.println("cc : " + car4.cc);
     }
 }
