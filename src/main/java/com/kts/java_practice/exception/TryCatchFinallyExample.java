79f1faa060d2c69433669a7177e4f0eab5fe7bd0
Kim tae-seop <kts3445@naver.com>
2024-05-06 16:43:03 +0900

    feat : Exception practice 예외 처리 연습

---
+++ src/main/java/com/kts/java_practice/exception/TryCatchFinallyExample.java
@@ -0,0 +1,12 @@
+package com.kts.java_practice.exception;
+
+// 일반 예외 처리
+public class TryCatchFinallyExample {
+    public static void main(String[] args) {
+        try {
+            Class clazz = Class.forName("java.lang.String2");
+        } catch (ClassNotFoundException e) {
+            System.out.println("클래스가 존재하지 않습니다.");
+        }
+    }
+}
