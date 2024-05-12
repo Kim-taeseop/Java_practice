c702986d5ca6f908cf0c18fb02ddac80e5f619c7
Kim tae-seop <kts3445@naver.com>
2024-05-12 20:18:26 +0900

    feat : Collection practice
    스택, 큐 연습

---
+++ src/main/java/com/kts/java_practice/Collection/Coin.java
@@ -0,0 +1,14 @@
+package com.kts.java_practice.Collection;
+
+// 스택 이용
+public class Coin {
+    private int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
